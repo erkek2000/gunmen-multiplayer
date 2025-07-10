e77d2c6e1bd72723899680bafc36c1d549d9dd5e
erkek2000 <mobulutnetas@gmail.com>
2025-07-10 16:59:15 +0300

    Added Connections and Visuals
    - Can connect to server.
    - Connected players are given ids and held in GameManager Dict.
    - Better visuals for test scene.
    - Can start 2 instances to test.
    - Players can give themselves names.

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,8 +1,7 @@
 extends Control
-
 @export var Adress = "127.0.0.1"
 @export var Port = 8910
-var peer
+var Peer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,44 +10,75 @@ func _ready():
 	multiplayer.connected_to_server.connect(connected_to_server)
 	multiplayer.connection_failed.connect(connection_failed)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 # Gets called on server and clients
 func peer_connected(id):
-	print("Player Connected: " + id)
+	print("Player Connected: ", id)
 
 # Gets called on server and clients
 func peer_disconnected(id):
-	print("Player Disconnected: " + id)
+	print("Player Disconnected: ", id)
 	
 # Called only from clients
-func connected_to_server(id):
+func connected_to_server():
 	print("Connected to Server!")
+	sendPlayerInfo.rpc_id(1, $VBoxContainer/LineEdit.text, multiplayer.get_unique_id())
 
 # Called only from clients
-func connection_failed(id):
+func connection_failed():
 	print("Connection Failed!")
 
+@rpc("any_peer")
+func sendPlayerInfo(name, id):
+	print("Received player info: ", name, " with ID: ", id)
+	if !GameManager.Players.has(id):
+		GameManager.Players[id] = {
+			"name" : name,
+			"id": id,
+			"score" : 0
+		}
+		print("Added player to dictionary. Total players: ", GameManager.Players.size())
+	
+	if multiplayer.is_server():
+		print("Server broadcasting all player info...")
+		for i in GameManager.Players:
+			sendPlayerInfo.rpc(GameManager.Players[i].name, i)
+			
+@rpc("any_peer","call_local")
+func startGame():	
+	var scene = load("res://scenes/Test_Scene.tscn").instantiate()
+	get_tree().root.add_child(scene)
+	self.hide()
+
 func _on_host_button_pressed():
-	peer = ENetMultiplayerPeer.new()
+	Peer = ENetMultiplayerPeer.new()
 	# Max clients = 2
-	var error = peer.create_server(Port, 2)
+	var error = Peer.create_server(Port, 2)
 	if error != OK:
-		print ("Error hosting the Server: " + error)
+		print ("Error hosting the Server: ", error)
 		return
 	
 	# Compress methods use CPU versus Bandwidth.
-	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
-	multiplayer.set_multiplayer_peer(peer)
+	# Compress method needs to be the SAME across the board.
+	Peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(Peer)
 	print("Waiting for players...")
-
+	
+	# Add the host player to the dictionary
+	var host_name = $VBoxContainer/LineEdit.text
+	if host_name == "":
+		host_name = "Host"
+	sendPlayerInfo(host_name, multiplayer.get_unique_id())
 
 func _on_join_button_pressed():
-	pass # Replace with function body.
-
+	Peer = ENetMultiplayerPeer.new()
+	Peer.create_client(Adress, Port)
+	Peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(Peer)
 
 func _on_start_button_pressed():
+	startGame.rpc()
 	pass # Replace with function body.
